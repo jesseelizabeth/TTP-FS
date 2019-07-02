4bdf4012c9ffcba3962260b4b1c29b236758becd
Jesse Evans <jesseelizabeth1@gmail.com>
2019-07-02 09:38:13 -0400

    portfolio reducer

--- client/store/portfolio.js
+++ client/store/portfolio.js
@@ -4,3 +4,26 @@ import axios from 'axios';
 const GET_STOCKS = 'GET_STOCKS';
 
 // action creators
+const gotStocks = stocks => ({
+  type: GET_STOCKS,
+  stocks,
+});
+
+// thunk
+export const fetchPortfolio = () => async dispatch => {
+  const { data } = await axios.get('/api/portfolio');
+  dispatch(gotStocks(data));
+};
+
+// initial state
+const initalState = [];
+
+// reducer
+export default function(state = initalState, action) {
+  switch (action.type) {
+    case GET_STOCKS:
+      return action.stocks;
+    default:
+      return state;
+  }
+}
